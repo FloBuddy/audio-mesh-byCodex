8e46893d0ae45011e7bb0d1d1af6cd34c9ae48c1
Flo <122550965+FloBuddy@users.noreply.github.com>
2026-04-24 10:57:25 +0100

    Initial Commit

---
+++ audio-mesh-byCodex/audio_mesh_byCodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_mesh_byCodexApp.swift
+//  audio-mesh-byCodex
+//
+//  Created by Florin Ilie on 24.04.26.
+//
+
+import SwiftUI
+
+@main
+struct audio_mesh_byCodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ audio-mesh-byCodexTests/audio_mesh_byCodexTests.swift
@@ -0,0 +1,18 @@
+//
+//  audio_mesh_byCodexTests.swift
+//  audio-mesh-byCodexTests
+//
+//  Created by Florin Ilie on 24.04.26.
+//
+
+import Testing
+
+struct audio_mesh_byCodexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
